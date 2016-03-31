6dbd8f8289a79c8a593e572f58f1e2c6a25d522a
Saffron <saffronbeatson@gmail.com>
2016-03-31 11:14:10 +0200

    first 3 Avo tests passing

--- avo.js
+++ avo.js
@@ -2,3 +2,38 @@ exports.deals = function(string) {
   var avoDeals = string.split(', ');
   return avoDeals
 }
+
+exports.avoPrices = function (avoDeals) {
+var pricePerAvo = [];
+
+for (var i=0; i<avoDeals.length; i++) {
+  //looked at previous work to remember naming avoDeals[i] a new var (deal)
+  var eachDeal = avoDeals[i];
+  var priceArray = eachDeal.split("for R");
+  var divideIndex= (priceArray[1] / priceArray[0]).toFixed(2)
+  pricePerAvo.push(divideIndex);
+
+}
+
+return pricePerAvo;
+}
+
+//here I asked questions like can you push an array into an object? How do I map the two arrays together? I know I have to loop through one of them.
+exports.avoMap = function (avoDeals) {
+  //I remember Andre & Steven saying this is not the best way to import a function into another one, but I've forgotten how to do it the way they suggested.
+  var avoPrices = exports.avoPrices;
+  var pricePerAvo = avoPrices(avoDeals);
+
+priceMap = {};
+
+for (var i= 0; i<avoDeals.length; i++) {
+  //I looked back at previuos work to remind myself that I had to phrase it avoDeals[i] and not avoDeals(i)
+var deal = avoDeals[i];
+var price = pricePerAvo [i];
+//At first I had an error where the priceMap was returning the deal still in an array. After fiddling around I found out this was because I at first stated priceMap[price]= [deal];
+  priceMap[price]= deal;
+}
+
+return priceMap;
+
+}
--- test/avo_test.js
+++ test/avo_test.js
@@ -4,10 +4,27 @@ var avo = require('../avo');
 describe("For the bestdeal kata,", function() {
 
 
-      it('I should list the different deals', function() {
+  it('I should list the different deals', function() {
 
-        var result = avo.deals('1 for R3, 2 for R7, 3 for R10, 5 for R14.50');
-        assert.deepEqual(result, ['1 for R3', '2 for R7', '3 for R10', '5 for R14.50']);
+    var result = avo.deals('1 for R3, 2 for R7, 3 for R10, 5 for R14.50');
+    assert.deepEqual(result, ['1 for R3', '2 for R7', '3 for R10', '5 for R14.50']);
 
-      });
-        });
+  });
+  it('I should calculate the price per avo for each deal', function() {
+    var result = avo.avoPrices(['1 for R3', '2 for R7', '3 for R10', '5 for R14.50']);
+    assert.deepEqual(result, [3.00, 3.50, 3.33, 2.90]);
+
+  });
+  // I looked back at my previous work to remember that my resulting object had to be in quotation marks.
+  it('I should create a price map- mapping the price per avo to the corresponding deal', function() {
+    var result = avo.avoMap(['1 for R3', '2 for R7', '3 for R10', '5 for R14.50']);
+    assert.deepEqual(result, {
+      '3.00': '1 for R3',
+      '3.50': '2 for R7',
+      '3.33': '3 for R10',
+      '2.90': '5 for R14.50',
+    });
+
+
+  });
+});
